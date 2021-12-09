3f0bdcedd14c503e957fc1ba83e81f5446164383
shashidhar <59072743+saka-8@users.noreply.github.com>
2021-12-10 00:42:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>shashidhara's</code>PortFolio.
+          This is shashidhara's PortFolio.
         </p>
         {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
